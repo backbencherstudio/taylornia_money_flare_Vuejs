3da8e0399fe16b6a4bd4fba84d91d6eac5ef65db
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-02-16 11:20:43 +0600

    Signup page  completed

--- app/components/Auth/SignUpForm.vue
+++ app/components/Auth/SignUpForm.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import EmailIcon from "../Icons/EmailIcon.vue";
+import LockIcon from "../Icons/LockIcon.vue";
+import PhoneIcon from "../Icons/PhoneIcon.vue";
 import SingleUserIcon from "../Icons/SingleUserIcon.vue";
 import BaseButton from "../Reusable/BaseButton.vue";
 
@@ -8,12 +11,12 @@ const handleSignUp = () => {};
 <template>
   <div>
     <!-- Image section -->
-    <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-20 p-6">
+    <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-10 xl:gap-20 p-6">
       <div class="image-wrapper">
         <img
           src="/images/signup.png"
           alt="Sign up image"
-          class="lg:h-[100vh] object-cover"
+          class="lg:h-screen object-cover"
         />
       </div>
       <div class="relative z-20 xl:w-126.5 lg:pr-4 xl:pr-0">
@@ -64,8 +67,8 @@ const handleSignUp = () => {};
                   placeholder="Enter Your Phone"
                   class="w-full px-8 py-4 rounded-xl bg-[#08080880] border border-[#808080] focus:text-[#2BFFC3] focus:outline-none focus:border focus:border-[#2BFFC3] focus:placeholder:text-[#2BFFC3] placeholder:text-sm"
                 />
-                <span class="absolute left-4 top-1/2 transform -translate-y-1/2"
-                  ><SingleUserIcon
+                <span class="absolute left-3 top-1/2 transform -translate-y-1/2"
+                  ><PhoneIcon
                 /></span>
               </div>
             </div>
@@ -79,8 +82,8 @@ const handleSignUp = () => {};
                   placeholder="Enter Your Email"
                   class="w-full px-8 py-4 rounded-xl bg-[#08080880] border border-[#808080] focus:text-[#2BFFC3] focus:outline-none focus:border focus:border-[#2BFFC3] focus:placeholder:text-[#2BFFC3] placeholder:text-sm"
                 />
-                <span class="absolute left-4 top-1/2 transform -translate-y-1/2"
-                  ><SingleUserIcon
+                <span class="absolute left-3 top-1/2 transform -translate-y-1/2"
+                  ><EmailIcon
                 /></span>
               </div>
             </div>
@@ -94,8 +97,8 @@ const handleSignUp = () => {};
                   placeholder="Enter Your Password"
                   class="w-full px-8 py-4 rounded-xl bg-[#08080880] border border-[#808080] focus:text-[#2BFFC3] focus:outline-none focus:border focus:border-[#2BFFC3] focus:placeholder:text-[#2BFFC3] placeholder:text-sm"
                 />
-                <span class="absolute left-4 top-1/2 transform -translate-y-1/2"
-                  ><SingleUserIcon
+                <span class="absolute left-3 top-1/2 transform -translate-y-1/2"
+                  ><LockIcon
                 /></span>
               </div>
             </div>
@@ -109,56 +112,52 @@ const handleSignUp = () => {};
                   placeholder="Confirm Your Password"
                   class="w-full px-8 py-4 rounded-xl bg-[#08080880] border border-[#808080] focus:text-[#2BFFC3] focus:outline-none focus:border focus:border-[#2BFFC3] focus:placeholder:text-[#2BFFC3] placeholder:text-sm"
                 />
-                <span class="absolute left-4 top-1/2 transform -translate-y-1/2"
-                  ><SingleUserIcon
-                /></span>
+                <span class="absolute left-3 top-1/2 transform -translate-y-1/2"
+                  ><LockIcon /></span>
               </div>
             </div>
           </div>
-          
-            <div class="flex items-center gap-4 mb-10">
-              <label class="neon-checkbox">
-                <input type="checkbox" />
-                <div class="neon-checkbox__frame">
-                  <div class="neon-checkbox__box">
-                    <div class="neon-checkbox__check-container">
-                      <svg viewBox="0 0 24 24" class="neon-checkbox__check">
-                        <path d="M3,12.5l7,7L21,5"></path>
-                      </svg>
-                    </div>
-                    <div class="neon-checkbox__glow"></div>
-                    <div class="neon-checkbox__borders">
-                      <span></span><span></span><span></span><span></span>
-                    </div>
+
+          <div class="flex items-center gap-4 mb-10">
+            <label class="neon-checkbox">
+              <input type="checkbox" />
+              <div class="neon-checkbox__frame">
+                <div class="neon-checkbox__box">
+                  <div class="neon-checkbox__check-container">
+                    <svg viewBox="0 0 24 24" class="neon-checkbox__check">
+                      <path d="M3,12.5l7,7L21,5"></path>
+                    </svg>
                   </div>
-                  <div class="neon-checkbox__effects">
-                    <div class="neon-checkbox__particles">
-                      <span></span><span></span><span></span><span></span>
-                      <span></span><span></span><span></span><span></span>
-                      <span></span><span></span><span></span><span></span>
-                    </div>
-                    <div class="neon-checkbox__rings">
-                      <div class="ring"></div>
-                      <div class="ring"></div>
-                      <div class="ring"></div>
-                    </div>
-                    <div class="neon-checkbox__sparks">
-                      <span></span><span></span><span></span><span></span>
-                    </div>
+                  <div class="neon-checkbox__glow"></div>
+                  <div class="neon-checkbox__borders">
+                    <span></span><span></span><span></span><span></span>
                   </div>
                 </div>
-              </label>
-              <p class="text-sm lg:text-lg">
-                Remember me for 30 days
-              </p>
-            </div>
+                <div class="neon-checkbox__effects">
+                  <div class="neon-checkbox__particles">
+                    <span></span><span></span><span></span><span></span>
+                    <span></span><span></span><span></span><span></span>
+                    <span></span><span></span><span></span><span></span>
+                  </div>
+                  <div class="neon-checkbox__rings">
+                    <div class="ring"></div>
+                    <div class="ring"></div>
+                    <div class="ring"></div>
+                  </div>
+                  <div class="neon-checkbox__sparks">
+                    <span></span><span></span><span></span><span></span>
+                  </div>
+                </div>
+              </div>
+            </label>
+            <p class="text-sm lg:text-lg">Remember me for 30 days</p>
+          </div>
 
-          
           <BaseButton name="Sign Up" type="submit" />
         </form>
         <div class="mt-5 xl:mt-10 text-center">
           Don’t have an account?
-          <NuxtLink to="/auth/signup" class="text-primary">Sign Up</NuxtLink>
+          <NuxtLink to="/auth/login" class="text-primary">Sign Up</NuxtLink>
         </div>
       </div>
     </div>
