f282cba3ac847c78ece1195c6ae151b250c81315
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-02-21 12:44:08 +0600

    Plans card added

---
+++ app/pages/(client)/aI-trading-plans/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AITradingPlans from "~/components/AITradingPlans/AITradingPlans.vue";
+import InvestSection from "~/components/HomeSections/InvestSection.vue";
+
+definePageMeta({
+  layout: "client",
+});
+</script>
+
+<template>
+  <div>
+    <AITradingPlans />
+    <InvestSection/>
+  </div>
+</template>
+
+<style scoped></style>
