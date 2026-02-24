592fb0fc39793a5ad3cc0b2f84e51da0667a6c1b
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-02-24 13:11:54 +0600

    Updated

---
+++ app/components/Icons/HexaIcon.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <g opacity="0.7">
+      <path
+        d="M9.98 4.88C10.965 4.292 11.458 4 12 4C12.542 4 13.034 4.293 14.02 4.88L16.98 6.638C17.965 7.224 18.459 7.518 18.73 8C19.001 8.482 19 9.069 19 10.241V13.759C19 14.931 19 15.517 18.73 16C18.459 16.483 17.965 16.776 16.98 17.362L14.02 19.121C13.035 19.707 12.541 20 12 20C11.459 20 10.966 19.707 9.98 19.12L7.02 17.362C6.035 16.776 5.541 16.482 5.27 16C4.999 15.518 5 14.931 5 13.759V10.24C5 9.07 5 8.483 5.27 8C5.541 7.517 6.035 7.224 7.02 6.638L9.98 4.88Z"
+        stroke="#080808"
+        stroke-width="1.5"
+      />
+      <path
+        opacity="0.5"
+        d="M5 8L2 6M19 8L22 6M5 16L2 18"
+        stroke="#080808"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        opacity="0.5"
+        d="M12 16.884L16 14.5"
+        stroke="#080808"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </g>
+  </svg>
+</template>
+
+<style scoped></style>
