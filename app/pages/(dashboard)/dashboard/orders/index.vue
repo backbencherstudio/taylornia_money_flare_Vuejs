479fcfdd03c2fcb7129b933764e7e4261e5bf554
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-03-02 16:21:34 +0600

    Order page completed

---
+++ app/pages/(dashboard)/dashboard/orders/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Orders from "~/components/DashboardComponenets/Overview/_Components/Orders/Orders.vue";
+
+definePageMeta({
+  layout: "dashboard",
+});
+</script>
+
+<template>
+  <div>
+    <Orders />
+  </div>
+</template>
+
+<style scoped></style>
