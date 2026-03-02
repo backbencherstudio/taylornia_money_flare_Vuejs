259967ffe7eede73f60b73c806c20e6c1db29a31
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-03-02 11:44:59 +0600

    Deposite for added in wallets

---
+++ app/components/Icons/Wallet2.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+  >
+    <path
+      d="M4.75 6.87511C4.75 7.36011 5.12501 7.75011 5.58501 7.75011H6.52499C6.92499 7.75011 7.25 7.41012 7.25 6.98512C7.25 6.53012 7.05001 6.36512 6.75501 6.26012L5.25 5.73512C4.955 5.63012 4.755 5.47012 4.755 5.01012C4.755 4.59012 5.08 4.24512 5.48 4.24512H6.42C6.88 4.24512 7.25501 4.63512 7.25501 5.12012"
+      stroke="#CCCCCC"
+      stroke-width="0.84"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M6 3.75V8.25"
+      stroke="#CCCCCC"
+      stroke-width="0.9"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M11 6C11 8.76 8.76 11 6 11C3.24 11 1 8.76 1 6C1 3.24 3.24 1 6 1"
+      stroke="#CCCCCC"
+      stroke-width="0.9"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M8.5 1.5V3.5H10.5"
+      stroke="#CCCCCC"
+      stroke-width="0.9"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M11 1L8.5 3.5"
+      stroke="#CCCCCC"
+      stroke-width="0.9"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<style scoped></style>
---
+++ app/pages/(dashboard)/dashboard/Wallets/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DepositeAndActivity from "~/components/DashboardComponenets/Overview/_Components/Wallets/DepositeAndActivity.vue";
+import DashboardUserHeader from "~/components/Shared/DashboardUserHeader.vue";
+
+definePageMeta({
+  layout: "dashboard",
+});
+</script>
+
+<template>
+  <div>
+    <DashboardUserHeader />
+    <DepositeAndActivity/>
+  </div>
+</template>
+
+<style scoped></style>
